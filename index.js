ec85b48290357c1fdf5e9adcec0c3516a0959222
pksingh <premrathore654@gmail.com>
2024-10-09 13:12:27 +0530

    js

---
+++ index.js
@@ -0,0 +1,211 @@
+// variable
+
+// what is variable
+
+// its used to hold data and can store in ram memory
+
+// let
+// var
+// const 44444
+
+// how to declare a variable
+
+// let a;
+// var b;
+
+// let hello = 10;
+
+// hello =  500;
+// window.console.log(hello)
+// hello  =  10;
+
+// hello  = "hello world"
+
+// window.console.log(hello)
+
+// assignment operator
+// =
+
+// datatype
+// string
+// number
+// boolean
+// Date
+// undefined
+// null
+// array
+// object
+
+// what is data type
+//  datatype  is used to define variable and reserver memory space
+
+// let a;
+// console.log(a);
+
+// let a = 10;
+
+// let b =  "hello world";
+
+// let c =  true ;
+
+// let d =  [];
+
+// let e =  {};
+
+// "" '' ``
+
+// 10;
+
+// " 10"
+
+// "54546545456";
+
+// Emily Davis
+// let a =  "E02387"
+
+// let b =  "Emily Davis"
+
+// let c =  ['E02387', '4654']
+
+// let d =  ['E02387', 'Emily Davis', 'Sr. Manger']
+
+// let f =  {
+//     id: "E02387",
+//     name: "Emily Davis"
+// }
+
+// let data =  [
+//     {},
+//     {},
+//     {},
+//     {},
+//     {},
+//     {},
+//     {},
+//     {},
+//     {},
+//     {},
+//     {},
+// ]
+
+//  operators
+
+//!  + = - * % > < <= >= ||  && !== == ===
+
+// let a = 10;
+// let b = 20;
+
+// let c;
+
+// c = a;
+
+// a = b;
+
+// b = c;
+
+// console.log(a);
+// console.log(b);
+
+// let a =  10;
+// let b  =  20;
+
+// let r =  [a, b] =  [b,a];
+// console.log(r);
+
+// let a = 10;
+
+// let b = "10";
+
+// let r = a = b;
+
+// let a =  10;
+
+// a /= 1;
+
+// let a  = 10;
+
+// a++;
+// a--;
+
+// console.log(a);
+
+// ||  &&    true false
+
+// let a =  10;
+// let b =  10;
+// let c =  20;
+// let d =  30;
+
+// let result =  a == b && c < d ;
+
+// console.log(result);
+
+// conditional statement
+
+// if()
+
+// if(){}
+
+// if(){
+
+// }else{
+
+// }
+
+// if(){
+
+// }esle if(){
+
+// }else{
+
+// }
+
+// let a = 18;
+// console.log("task one");
+// if (a == 1) {
+//   console.log("task done");
+//   console.log("task two");
+//   alert();
+// } else if (a == 18) {
+//   console.log("task done 18");
+//   alert(a);
+// } else {
+//   console.log("task three");
+// }
+// console.log("task fort");
+// console.log("task 5");
+
+// let grade = "z";
+// switch (grade) {
+//   case "b":
+//     console.log("grade b");
+//     break;
+//   case "a":
+//     console.log("grade a");
+//     break;
+//   case "c":
+//     console.log("grade b");
+//     break;
+//   default:
+//     console.log("no grade found");
+// }
+
+// let a  = 10;
+
+// let b =  "20";
+
+
+
+
+// 2. 
+
+let age  = 25;
+
+if(age >= 18 && age <=30){
+    console.log('u can fill the form');
+}else if(age === 17){
+    console.log('wait for 1 year');
+}
+else{
+    console.log("u cannot fill the form");
+}
